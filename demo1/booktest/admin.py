b992b36d44362609a864c9f820763d06467294ee
Swy02 <1148422127@qq.com>
2019-07-02 17:54:21 +0800

    django

--- demo1/booktest/admin.py
+++ demo1/booktest/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+
+admin.site.register(BookInfo,)
+
+
+
+class HeroInfoAdmin(admin.ModelAdmin):
+    list_display = ['name','content']
+
+
+admin.site.register(HeroInfo,HeroInfoAdmin)
